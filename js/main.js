435ccf84fe6594386208485a61f465ad49f6216e
kamar96 <123400871+kamar96@users.noreply.github.com>
2023-03-06 21:48:16 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var buttonMenu = document.querySelector('.banner_title-btn');
+var popup = document.querySelector('.popup');
+buttonMenu.addEventListener('click', function() {
+  popup.classList.add('popup-popupbg');
+});
